f7150aaa79a515984d2ce1336574bf5556b70580
mucahid <mucahidd3@gmail.com>
2022-04-13 22:04:23 +0300

    connect adapter with public holiday page

--- app/src/main/java/com/example/returnkotlin/ui/holiday/PublicHolidayFragment.kt
+++ app/src/main/java/com/example/returnkotlin/ui/holiday/PublicHolidayFragment.kt
@@ -1,33 +1,33 @@
 package com.example.returnkotlin.ui.holiday
 
-import android.util.Log
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.viewModels
 import com.example.returnkotlin.R
 import com.example.returnkotlin.base.BaseFragment
 import com.example.returnkotlin.base.ResourceStatus
 import com.example.returnkotlin.databinding.FragmentPublicHolidayBinding
+import com.example.returnkotlin.ui.list.CountryListAdapter
 import com.example.returnkotlin.util.extensions.changeWith
 import com.example.returnkotlin.util.extensions.hide
 import com.example.returnkotlin.util.extensions.show
-import com.example.returnkotlin.util.extensions.toStringList
 import com.example.returnkotlin.viewmodel.PublicHolidayViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
-private const val TAG : String = "PublicHolidayFragment.java"
+private const val TAG: String = "PublicHolidayFragment.java"
 
 @AndroidEntryPoint
-class PublicHolidayFragment : BaseFragment<FragmentPublicHolidayBinding,PublicHolidayViewModel>() {
+class PublicHolidayFragment : BaseFragment<FragmentPublicHolidayBinding, PublicHolidayViewModel>() {
 
-    override val mViewModel : PublicHolidayViewModel by viewModels()
+    override val mViewModel: PublicHolidayViewModel by viewModels()
 
     override fun bindLayoutId(): Int = R.layout.fragment_public_holiday
+
+    lateinit var mAdapter: PublicHolidayAdapter
 
     override fun initViews() {
 
         mBinding.viewModel = mViewModel
-        mBinding.searchStatusText.changeWith(getString(R.string.please_search))
-        mBinding.searchStatusText.show()
+        showSearchStatus(getString(R.string.please_search))
 
         mBinding.yearText.doAfterTextChanged {
             mViewModel.updateYearText(it?.toString())
@@ -38,7 +38,7 @@ class PublicHolidayFragment : BaseFragment<FragmentPublicHolidayBinding,PublicHo
         }
 
         mViewModel.getResource().observe(viewLifecycleOwner) {
-            when(it.status) {
+            when (it.status) {
                 ResourceStatus.PROGRESS -> {
                     mBinding.searchStatusText.hide()
                     mBinding.holidayRV.hide()
@@ -48,14 +48,22 @@ class PublicHolidayFragment : BaseFragment<FragmentPublicHolidayBinding,PublicHo
                     mBinding.progressBar.hide()
                     mBinding.searchStatusText.hide()
                     mBinding.holidayRV.show()
+                    mAdapter = PublicHolidayAdapter(it.data!!)
+                    mBinding.holidayRV.adapter = mAdapter
                 }
                 ResourceStatus.ERROR -> {
                     mBinding.progressBar.hide()
                     mBinding.holidayRV.hide()
-                    mBinding.searchStatusText.changeWith(getString(R.string.something_went_wrong))
-                    mBinding.searchStatusText.show()
+                    showSearchStatus(getString(R.string.something_went_wrong))
                 }
             }
+        }
+    }
+
+    private fun showSearchStatus(message: String?) {
+        mBinding.let {
+            it.searchStatusText.changeWith(message)
+            it.searchStatusText.show()
         }
     }
 
