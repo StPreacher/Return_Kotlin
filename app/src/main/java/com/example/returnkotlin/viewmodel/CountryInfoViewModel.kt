b484ff10f1f2632ce0da0f6d8ea6925d27464afe
mucahid <mucahidd3@gmail.com>
2022-04-16 00:08:56 +0300

    finish viewmodel function that using for get data

--- app/src/main/java/com/example/returnkotlin/viewmodel/CountryInfoViewModel.kt
+++ app/src/main/java/com/example/returnkotlin/viewmodel/CountryInfoViewModel.kt
@@ -1,14 +1,36 @@
 package com.example.returnkotlin.viewmodel
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.returnkotlin.base.Resource
+import com.example.returnkotlin.base.ResourceError
+import com.example.returnkotlin.base.ResourceStatus
+import com.example.returnkotlin.model.CountryInfo
 import com.example.returnkotlin.repo.CountryInfoRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class CountryInfoViewModel @Inject constructor(private val repository: CountryInfoRepository) {
+class CountryInfoViewModel @Inject constructor(private val repository: CountryInfoRepository) :
+    ViewModel() {
+
+    val resource = MutableLiveData<Resource<CountryInfo>>()
 
     fun getCountryInfo(countryCode: String) {
-
+        resource.postValue(Resource(ResourceStatus.PROGRESS))
+        viewModelScope.launch(Dispatchers.IO) {
+            val response = repository.getCountryInfo(countryCode)
+            if (response.isSuccessful) {
+                response.let {
+                    resource.postValue(Resource(ResourceStatus.SUCCESS,it.body()))
+                }
+            } else {
+                resource.postValue(Resource(ResourceStatus.ERROR,null, ResourceError(61,response.message())))
+            }
+        }
     }
 
 }
